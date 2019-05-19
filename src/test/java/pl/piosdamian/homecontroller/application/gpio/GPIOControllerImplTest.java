92857cb724699f4e4e9e7298eeb47b4c09f3067a
Damian Pioś <piosdamian@gmail.com>
2019-05-19 23:38:52 +0200

    test configuration

--- src/test/java/pl/piosdamian/homecontroller/application/gpio/GPIOControllerImplTest.java
+++ src/test/java/pl/piosdamian/homecontroller/application/gpio/GPIOControllerImplTest.java
@@ -1,16 +1,14 @@
 package pl.piosdamian.homecontroller.application.gpio;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.junit4.SpringRunner;
+import pl.piosdamian.homecontroller.application.configuration.GpioConfiguration;
 
 import java.io.IOException;
-
-import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -20,11 +18,14 @@ public class GPIOControllerImplTest {
     @Autowired
     private GPIOController controller;
 
+    @Autowired
+    private GpioConfiguration gpioConfiguration;
+
     @Test
-    @Ignore
     //manual test
-    public void blink() throws IOException {
+    public void blink() throws IOException, InterruptedException {
         controller.registerSwitcher(29, "LED", true);
         controller.blink(29);
+        Thread.sleep(this.gpioConfiguration.getOutput().getBlinkDuration());
     }
 }
