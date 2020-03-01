16d95253477a0fbf90d58ee54e85a5d6d176419a
Damian Pioś <piosdamian@gmail.com>
2020-03-01 22:31:41 +0100

    java 11, more logs

--- src/main/java/pl/piosdamian/homecontroller/application/communication/EventPushService.java
+++ src/main/java/pl/piosdamian/homecontroller/application/communication/EventPushService.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
@@ -15,7 +16,7 @@ public class EventPushService implements Observable, Broadcaster {
 
     private Map<String, SseEmitter> observers = new HashMap<>();
     private List<String> deadObservers = new ArrayList<>();
-    @Value("${emitter.source.timeout: 600000}")
+    @Value("${emitter.source.timeout: 3600000}")
     private long sseEmitterTimeout;
 
     @Override
@@ -28,7 +29,7 @@ public class EventPushService implements Observable, Broadcaster {
             try {
                 emitter.send(eventBuilder);
             } catch (IOException e) {
-                log.warn("Emitter with id {} thrown IOException, {}, will be removed", id, e.getMessage());
+                log.warn("Emitter with id {} thrown IOException: {}, will be removed", id, e.getMessage());
                 this.deadObservers.add(id);
             }
         });
@@ -39,14 +40,21 @@ public class EventPushService implements Observable, Broadcaster {
     public SseEmitter addObserver(String id) {
         final SseEmitter emitter = new SseEmitter(this.sseEmitterTimeout);
         emitter.onCompletion(() -> this.removeObserver(id));
-        emitter.onTimeout(() -> this.removeObserver(id));
+        emitter.onTimeout(() -> {
+            log.warn("Timeout occurred for observer {}", id);
+            this.removeObserver(id);
+        });
+        emitter.onError((error) -> {
+            log.warn(error.getMessage());
+            this.removeObserver(id);
+        });
         this.observers.put(id, emitter);
         return emitter;
     }
 
     @Override
     public void removeObserver(String id) {
-        this.observers.remove(id);
+        Optional.ofNullable(this.observers.remove(id)).ifPresent(ResponseBodyEmitter::complete);
     }
 
     private void cleanDeadObservers() {
